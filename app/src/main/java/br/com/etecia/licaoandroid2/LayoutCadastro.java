5ae9ca00d67e96066c4083f4215fb96c7187f8d9
Caioba24 <dipietroapg@gmail.com>
2022-04-04 19:58:15 -0300

    Primeiro Commit
    *Icones, primeiros botões e EditText*

---
+++ app/src/main/java/br/com/etecia/licaoandroid2/LayoutCadastro.java
@@ -0,0 +1,14 @@
+package br.com.etecia.licaoandroid2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LayoutCadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_cadastro);
+    }
+}
