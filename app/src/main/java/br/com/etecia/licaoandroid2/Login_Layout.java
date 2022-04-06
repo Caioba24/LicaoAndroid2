24801032b59fec6843187dd414c3966db0c1e6d4
Caioba24 <dipietroapg@gmail.com>
2022-04-06 17:37:06 -0300

    *Quase Finalizando as telas*

--- app/src/main/java/br/com/etecia/licaoandroid2/Login_Layout.java
+++ app/src/main/java/br/com/etecia/licaoandroid2/Login_Layout.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LoginLayout extends AppCompatActivity {
+public class Login_Layout extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_layout);
+        setContentView(R.layout.layout_login);
     }
 }
